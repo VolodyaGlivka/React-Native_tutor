c3f98f5c512484969bc631bea5107ed77db82cd2
volodya19971 <mega-volodya.glivka@ukr.net>
2020-01-13 20:57:18 +0200

    add-basic--add-delete-operation-&-modal-v1

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, TouchableOpacity, Modal } from 'react-native';
 
 export default function App() {
-  const [title, setTitle] = React.useState('Open up App.js to start working on your app!');
+  const [goalList, setGoalList] = useState([]);
+  const [goalItem, setGoalitem] = useState();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const goalInputhandler = goalInputItem => {
+    setGoalitem(goalInputItem);
+  };
+  const addGoallHandler = () => {
+    setGoalList([...goalList, { id: Math.random().toString(), value: goalItem }]);
+    setIsModalVisible(false);
+    setGoalitem('');
+  };
+  const onDelete = id => {
+    const newArr = goalList.filter(goal => goal.id !== id);
+    setGoalList([...newArr]);
+  };
+  const closeModal = () => {
+    setIsModalVisible(false);
+    setGoalitem('');
+  }
   return (
     <View style={styles.container}>
-      <Text>{title}</Text>
-      <Button title="Change text" onPress={()=>setTitle('changed text')}/>
+      <Button title="Open Modal" onPress={() => setIsModalVisible(true)} />
+      <Modal visible={isModalVisible} animationType="slide">
+        <View style={{ ...styles.flex, ...styles.padding30 }}>
+          <TextInput placeholder="Enter goal" value={goalItem} onChangeText={goalInputhandler} />
+          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+            <Button title="Add" onPress={addGoallHandler} />
+            <Button title="Close" color="red" onPress={closeModal} />
+          </View>
+        </View>
+      </Modal>
+      <FlatList
+        data={goalList}
+        keyExtractor={(item, index) => item.id}
+        renderItem={itemData => (
+          <TouchableOpacity activeOpacity={0.8} onPress={() => onDelete(itemData.item.id)}>
+            <View
+              style={{ padding: 15, marginVertical: 10, borderWidth: 1, borderColor: 'black', backgroundColor: '#ccc' }}
+            >
+              <Text>{itemData.item.value}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30
   },
+  flex: {
+    display: 'flex'
+  },
+  padding30: {
+    padding: 30
+  }
 });
